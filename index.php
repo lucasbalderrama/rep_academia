fb688c3a68b9b55c17338d80106698e2cfa8575c
Gabriel-Britoo <gabrito343@gmail.com>
2025-01-30 13:34:00 -0300

    Mudanças

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/vitalis-logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/home.css">
+    <title>Vitalis - Início</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a id="inicio" href="index.php">Início</a></li>
+                <li><a href="aluno.php">Página do aluno</a></li>
+                <li><a href="instrutor.php">Página do instrutor </a></li>
+                <li><a href="aulas.php">Aulas</a></li>
+            </ul>
+        </nav>        
+    </header>
+    <main>
+        <section id='secao-banner'>
+            <img src="img/vitalis-banner.png" alt="">
+            <div>
+                <p>Além de frequentar a academia diariamente, é possível marcar aulas particulares com profissionais qualificados na área que desejar, seja ela musculação, crossfit, yoga e diversas outras!</p>
+                <a href="aula.php"><button>AGENDAR AULA</button></a>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
